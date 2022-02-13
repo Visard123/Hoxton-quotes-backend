acb6ba801cafd67d2663afaf596dc83ae9e35ace
Visard123 <92378096+Visard123@users.noreply.github.com>
2022-02-14 00:30:10 +0100

    errors

---
+++ db.ts
@@ -0,0 +1,42 @@
+type Quote = {
+  id: number;
+  quote: string;
+  author: string;
+};
+export const quotes: Quote = [
+  {
+    id: 1,
+    quote: `The greatest glory in living lies not in never falling, but in rising every time we fall. `,
+    author: `Nelson Mandela`,
+  },
+  {
+    id: 2,
+    quote: `The way to get started is to quit talking and begin doing. `,
+    author: `Walt Disney`,
+  },
+  {
+    id: 3,
+    quote: `Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking. `,
+    author: `Steve Jobs`,
+  },
+  {
+    id: 4,
+    quote: `If life were predictable it would cease to be life, and be without flavor. `,
+    author: `Eleanor Roosevelt`,
+  },
+  {
+    id: 5,
+    quote: `If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. `,
+    author: `Oprah Winfrey`,
+  },
+  {
+    id: 6,
+    quote: `If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success. `,
+    author: `James Cameron`,
+  },
+  {
+    id: 7,
+    quote: `Life is what happens when you're busy making other plans. `,
+    author: `John Lennon`,
+  },
+];
--- index.ts
+++ index.ts
@@ -1,1 +1,48 @@
-console.log("hey");
+import express, { request, response } from "express";
+import { quotes } from "./db";
+const app = express();
+const PORT = 4000;
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+app.get("/", (req, res) => {
+  res.send(`
+    <h1 style="color: red;">Welcome to our quotes API!</h1>
+    <p>Here are some endpoints you can use:</p>
+    <ul>
+      <li><a href="/quotes">/quotes</a></li>
+      <li><a href="/randomQuote">/randomQuote</a></li>
+    </ul>
+   `);
+});
+
+app.get(`/quotes`, (request, response) => {
+  response.send(`/quotes`);
+});
+console.log(quotes);
+
+app.get("/randomQuote", (req, res) => {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const quote = quotes[randomIndex];
+  res.send(quote);
+});
+
+app.get("/quotes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const match = quotes.find((quote: { id: number }) => quote.id === id);
+  if (match) {
+    res.send(match);
+  } else {
+    res.status(404).send({ error: "Quote not found." });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server up on:http://localhost3000:${PORT} `);
+});
+function cors(arg0: { origin: string }): any {
+  throw new Error("Function not implemented.");
+}
